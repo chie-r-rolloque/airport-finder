9df7880058483b157a3ee7f9b753671a1c8b83e9
Ritchie R. Rolloque <chie@blisdigital.com>
2022-07-18 11:48:39 +0800

    Initial Project Files

---
+++ AirportFinder/AirportFinder/AirportFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportFinderApp.swift
+//  AirportFinder
+//
+//  Created by Ritchie R. Rolloque on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AirportFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
